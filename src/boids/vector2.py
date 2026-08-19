f47ec479a3c220ef745f7d6ff3f8238a1b7d4364
ewigael <311100398+ewigael@users.noreply.github.com>
2026-08-19 02:59:56 +0200

    Adds __str__() to Vector2

--- src/boids/vector2.py
+++ src/boids/vector2.py
@@ -5,6 +5,9 @@ class Vector2:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    
+    def __str__(self):
+        return f"Vector2(x = {self.x}, y = {self.y})"
 
     def __add__(self, other):
         return Vector2(self.x + other.x, self.y + other.y)
