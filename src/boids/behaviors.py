9006c1b4522cd1332142da599d4e4e240a1e49af
ewigael <311100398+ewigael@users.noreply.github.com>
2026-08-19 18:48:23 +0200

    Adds alignement behavior

--- src/boids/behaviors.py
+++ src/boids/behaviors.py
@@ -2,6 +2,8 @@
 
 from .vector2 import Vector2
 
+MAX_ALIGNMENT = 10
+
 
 def separation(boid, neighbors):
     steering = Vector2(0, 0)
@@ -18,12 +20,28 @@ def separation(boid, neighbors):
     return steering
 
 
+def alignment(boid, neighbors):
+    average_velocity = Vector2(0, 0)
+
+    for other in neighbors:
+        average_velocity += other.velocity
+
+    average_velocity /= len(neighbors)
+    steering = average_velocity - boid.velocity
+
+    if steering.length() > MAX_ALIGNMENT:
+        return steering.normalize() * MAX_ALIGNMENT
+    else:
+        return steering
+
+
 def flock(boid, neighbors):
     force = Vector2(0, 0)
 
     if not len(neighbors):
         return force
 
-    force += separation(boid, neighbors) * 100
+    force += separation(boid, neighbors) * 50
+    force += alignment(boid, neighbors) * 2
 
     return force
